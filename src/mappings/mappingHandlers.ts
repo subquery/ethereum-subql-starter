1c1b61ef8aa11cea353bf43f7a64809fb42ac781
Scott Twiname <skott.twiname@gmail.com>
2022-05-20 15:49:31 +1200

    Update deps

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -8,92 +8,10 @@ import {
   AvalancheReceiptEntity,
 } from "../types";
 import {
-//  AvalancheBlock,
-  AvalancheResult,
-//  AvalancheTransaction,
+  AvalancheBlock,
+  AvalancheTransaction,
+  AvalancheLog,
 } from "@subql/types-avalanche";
-
-export type AvalancheBlock = {
-  blockExtraData: string;
-  difficulty: bigint;
-  extDataGasUsed: string;
-  extDataHash: string;
-  gasLimit: bigint;
-  gasUsed: bigint;
-  hash: string;
-  logs: AvalancheLog[];
-  logsBloom: string;
-  miner: string;
-  mixHash: string;
-  nonce: string;
-  number: number;
-  parentHash: string;
-  receiptsRoot: string;
-  sha3Uncles: string;
-  size: bigint;
-  stateRoot: string;
-  timestamp: bigint;
-  totalDifficulty: bigint;
-  transactions: AvalancheTransaction[];
-  transactionsRoot: string;
-  uncles: string[];
-  baseFeePerGas?: bigint;
-  blockGasCost?: bigint;
-};
-
-export type AvalancheLog<T extends AvalancheResult = AvalancheResult> = {
-  address: string;
-  topics: string[];
-  data: string;
-  blockNumber: number;
-  transactionHash: string;
-  transactionIndex: number;
-  blockHash: string;
-  logIndex: number;
-  removed: boolean;
-  args?: T;
-};
-
-export type AvalancheReceipt = {
-  blockHash: string;
-  blockNumber: number;
-  contractAddress: string;
-  cumulativeGasUsed: bigint;
-  effectiveGasPrice: bigint;
-  from: string;
-  gasUsed: bigint;
-  logs: AvalancheLog[];
-  logsBloom: string;
-  status: boolean;
-  to: string;
-  transactionHash: string;
-  transactionIndex: number;
-  type: string;
-};
-
-export type AvalancheTransaction<T extends AvalancheResult = AvalancheResult> = {
-  blockHash: string;
-  blockNumber: number;
-  from: string;
-  gas: bigint;
-  gasPrice: bigint;
-  hash: string;
-  input: string;
-  nonce: bigint;
-  to: string;
-  transactionIndex: bigint;
-  value: bigint;
-  type: string;
-  v: bigint;
-  r: string;
-  s: string;
-  receipt: AvalancheReceipt;
-  accessList?: string[];
-  chainId?: string;
-  maxFeePerGas?: bigint;
-  maxPriorityFeePerGas?: bigint;
-  args?: T;
-};
 
 export async function handleBlock(block: AvalancheBlock): Promise<void> {
   const blockRecord = new AvalancheBlockEntity(block.hash);
