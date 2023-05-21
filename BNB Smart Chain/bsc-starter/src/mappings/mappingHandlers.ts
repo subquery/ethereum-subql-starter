ba595035135d30215b3957726f93e056cd98b7a2
bz888 <ziyu.ben158@gmail.com>
2023-05-22 10:18:27 +1200

    update bnb to strict

--- BNB Smart Chain/bsc-starter/src/mappings/mappingHandlers.ts
+++ BNB Smart Chain/bsc-starter/src/mappings/mappingHandlers.ts
@@ -3,9 +3,11 @@ import {
   ApproveTransaction,
   TransferLog,
 } from "../types/abi-interfaces/Erc20Abi";
+import assert from "assert";
 
 export async function handleLog(log: TransferLog): Promise<void> {
   logger.info(`New transfer transaction log at block ${log.blockNumber}`);
+  assert(log.args, "No log.args")
   const transfer = Transfer.create({
     id: log.transactionHash,
     blockHeight: log.blockNumber.toString(),
@@ -20,6 +22,7 @@ export async function handleLog(log: TransferLog): Promise<void> {
 
 export async function handleTransaction(tx: ApproveTransaction): Promise<void> {
   logger.info(`New Approval transaction at block ${tx.blockNumber}`);
+  assert(tx.args, "No tx.args")
   const approval = Approval.create({
     id: tx.hash,
     blockHeight: tx.blockNumber.toString(),
