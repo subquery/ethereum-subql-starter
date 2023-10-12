f168ebd4c9ac69098f81a83b64a5551238af6972
bz888 <ziyu.ben158@gmail.com>
2023-10-12 15:23:31 +1300

    resort 2

---
+++ Scroll/scroll-sepolia-starter/project.ts
@@ -0,0 +1,92 @@
+import {
+    EthereumProject,
+    EthereumDatasourceKind,
+    EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+    specVersion: "1.0.0",
+    version: "0.0.1",
+    name: "scroll-sepolia-starter",
+    description:
+        "This project can be use as a starting point for developing your new Scroll Sepolia testnet SubQuery project",
+    runner: {
+        node: {
+            name: "@subql/node-ethereum",
+            version: ">=3.0.0",
+        },
+        query: {
+            name: "@subql/query",
+            version: "*",
+        },
+    },
+    schema: {
+        file: "./schema.graphql",
+    },
+    network: {
+        /**
+         * chainId is the EVM Chain ID, for Scroll Sepolia this is 534351
+         * https://chainlist.org/chain/534351
+         */
+        chainId:
+            "534351",
+        /**
+         * This endpoint must be a public non-pruned archive node
+         * Public nodes may be rate limited, which can affect indexing speed
+         * When developing your project we suggest getting a private API key
+         * You can get them from OnFinality for free https://app.onfinality.io
+         * https://documentation.onfinality.io/support/the-enhanced-api-service
+         */
+        endpoint: ["https://scroll-sepolia.blockpi.network/v1/rpc/public"]
+        // Recommended to provide the HTTP endpoint of a full chain dictionary to speed up processing
+        // dictionary: "https://api.subquery.network/sq/subquery/scroll-sepolia-dictionary"
+    },
+    dataSources: [
+        {
+            kind: EthereumDatasourceKind.Runtime,
+            startBlock: 1, // This is the block that the contract was deployed on https://sepolia-blockscout.scroll.io/address/0x5300000000000000000000000000000000000004
+
+            options: {
+                // Must be a key of assets
+                abi:'erc20',
+                // This is the contract address for Wrapped Ether https://sepolia-blockscout.scroll.io/address/0x5300000000000000000000000000000000000004
+                address:'0x5300000000000000000000000000000000000004'
+            },
+            assets: new Map([
+                ['erc20', { file: "./abis/erc20.abi.json" }],
+            ]),
+            mapping: {
+                file: "./dist/index.js",
+                handlers: [
+                    {
+                        kind: EthereumHandlerKind.Call,
+                        handler: "handleTransaction",
+                        filter: {
+                            /**
+                             * The function can either be the function fragment or signature
+                             * function: '0x095ea7b3'
+                             * function: '0x7ff36ab500000000000000000000000000000000000000000000000000000000'
+                             */
+                            function: "approve(address spender, uint256 rawAmount)",
+                        },
+                    },
+                    {
+                        kind: EthereumHandlerKind.Event,
+                        handler: "handleLog",
+                        filter: {
+                            /**
+                             * Follows standard log filters https://docs.ethers.io/v5/concepts/events/
+                             * address: "0x60781C2586D68229fde47564546784ab3fACA982"
+                             */
+                            topics: ["Transfer(address indexed from, address indexed to, uint256 amount)"],
+                        },
+                    },
+                ],
+            },
+        },
+    ],
+    repository: "https://github.com/subquery/ethereum-subql-starter"
+};
+
+export default project;
