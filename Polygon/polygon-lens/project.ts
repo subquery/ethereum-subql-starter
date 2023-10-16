6e72c48301154f757a22dd52720bcdc120b0d7a6
bgodlin <37313677+bgodlin@users.noreply.github.com>
2023-10-16 03:15:50 +0200

    Lens Example Project Added (#47)
    * Lens Example Project Added
    
    * Bugs Fixed
    
    * Update Polygon/polygon-lens/src/mappings/mappingHandlers.ts
    
    * Update Polygon/polygon-lens/src/mappings/mappingHandlers.ts
    
    * Update Polygon/polygon-lens/src/mappings/mappingHandlers.ts
    
    * Update Polygon/polygon-lens/src/mappings/mappingHandlers.ts
    
    ---------
    
    Co-authored-by: James Bayly <46693720+jamesbayly@users.noreply.github.com>

---
+++ Polygon/polygon-lens/project.ts
@@ -0,0 +1,91 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "polygon-lens",
+  description:
+    "This project can be use as a starting point for developing your new polygon SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the EVM Chain ID, for Polygon this is 137
+     * https://chainlist.org/chain/137
+     */
+    chainId: "137",
+    /**
+     * This endpoint must be a public non-pruned archive node
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * You can get them from OnFinality for free https://app.onfinality.io
+     * https://documentation.onfinality.io/support/the-enhanced-api-service
+     */
+    endpoint: ["https://polygon.api.onfinality.io/public"],
+    // Recommended to provide the HTTP endpoint of a full chain dictionary to speed up processing
+    dictionary:
+      "https://gx.api.subquery.network/sq/subquery/polygon-dictionary",
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 28384641, // This is the block that the contract was deployed on
+      options: {
+        // Must be a key of assets
+        abi: "LensHub",
+        address: "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d",
+      },
+      assets: new Map([["LensHub", { file: "./abis/LensHub.abi.json" }]]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handlePostCreated",
+            filter: {
+              topics: [
+                "PostCreated(uint256,uint256,string,address,bytes,address,bytes,uint256)",
+              ],
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleProfileCreated",
+            filter: {
+              topics: [
+                "ProfileCreated(uint256,address,address,string,string,address,bytes,string,uint256)",
+              ],
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleFollowed",
+            filter: {
+              topics: ["Followed(address,uint256[],bytes[],uint256)"],
+            },
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
---
+++ Polygon/polygon-lens/src/mappings/mappingHandlers.ts
@@ -0,0 +1,110 @@
+import {
+  PostCreatedLog,
+  ProfileCreatedLog,
+  FollowedLog,
+} from "../types/abi-interfaces/LensHubAbi";
+import { Account, Post, Profile, Follow } from "../types";
+import assert from "assert";
+import { BigNumber } from "ethers";
+
+export async function getOrCreateAccount(
+  accountAddress: string
+): Promise<Account> {
+  let account = await Account.get(accountAddress);
+  if (!account) {
+    account = Account.create({
+      id: accountAddress,
+    });
+  }
+  return account;
+}
+
+export async function getOrCreateFollow(
+  accountAddress: string
+): Promise<Follow> {
+  let follow = await Follow.get(accountAddress);
+  if (!follow) {
+    follow = Follow.create({ id: accountAddress });
+  }
+  return follow;
+}
+
+export function getNewPublicactionId(profileId: BigInt, pubId: BigInt): string {
+  return profileId.toString().concat("-").concat(pubId.toString());
+}
+
+export async function getOrCreatePost(pubId: BigInt): Promise<Post> {
+  let post = await Post.get(pubId.toString());
+  if (!post) {
+    post = await Post.create({
+      id: pubId.toString(),
+    });
+  }
+  return post;
+}
+
+export async function getOrCreateProfile(profileId: string): Promise<Profile> {
+  let profile = await Profile.get(profileId);
+  if (!profile) {
+    profile = Profile.create({
+      id: profileId,
+    });
+  }
+  return profile;
+}
+
+export async function handleProfileCreated(
+  event: ProfileCreatedLog
+): Promise<void> {
+  logger.warn("Handling ProfileCreatedLog");
+  assert(event.args, "No log args");
+  logger.warn(event.args.profileId.toString());
+
+  let profile = await getOrCreateProfile(event.args.profileId.toString());
+  let creator = await getOrCreateAccount(event.args.creator);
+  let to = await getOrCreateAccount(event.args.to);
+  profile.creatorId = creator.id;
+  profile.ownerId = (await getOrCreateAccount(event.args.to)).id;
+  profile.followNFTURI = event.args.followNFTURI;
+  profile.handle = event.args.handle;
+  profile.imageURI = event.args.imageURI;
+  creator.save();
+  to.save();
+  profile.save();
+}
+
+export async function handlePostCreated(event: PostCreatedLog): Promise<void> {
+  logger.warn("Handling PostCreatedLog");
+  assert(event.args, "No log args");
+  let post = await getOrCreatePost(event.args.pubId.toBigInt());
+  let profile = await getOrCreateProfile(event.args.profileId.toString());
+  post.profileId = profile.id;
+  post.timestamp = event.args.timestamp.toBigInt();
+  post.contentURI = event.args.contentURI;
+  profile.save();
+  post.save();
+}
+
+export async function handleFollowed(event: FollowedLog): Promise<void> {
+  logger.warn("Handling FollowedLog");
+  assert(event.args, "No log args");
+  for (let index in event.args.profileIds) {
+    let profileId = event.args.profileIds[index];
+    logger.warn(profileId.toString());
+    let profile = await getOrCreateProfile(profileId.toString());
+    let follow = await getOrCreateFollow(
+      event.args.follower
+        .concat("-")
+        .concat(event.transaction.hash)
+        .concat("-")
+        .concat(profileId.toString())
+    );
+    let follower = await getOrCreateAccount(event.args.follower);
+    follow.fromAccountId = follower.id;
+    follow.toProfileId = profile.id;
+    follow.timestamp = event.args.timestamp.toBigInt();
+    profile.save();
+    follow.save();
+    follower.save();
+  }
+}
