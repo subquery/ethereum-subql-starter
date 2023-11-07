86e14aae1672cdf829ed6132a634488c8fa1a81f
James Bayly <bayly.james@gmail.com>
2023-11-07 13:37:03 +0800

    Add Roll Testnet

---
+++ Roll/roll-testnet-starter/project.ts
@@ -0,0 +1,88 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "roll-testnet-starter",
+  description:
+    "This project can be use as a starting point for developing your new Roll Testnet SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the EVM Chain ID, for Roll this is 1748
+     * https://chainlist.org/chain/1748
+     */
+    chainId: "1748",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     */
+    endpoint: ["https://roll.calderachain.xyz/http"],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 1800164, // This is the block that the contract was deployed on https://roll.calderaexplorer.xyz/tx/0x70b9c9c0fd562ba6e71b7e0aa70bb802462d5f6986eabfe1515647075ee898a8
+      options: {
+        // Must be a key of assets
+        abi: "erc20",
+        // This is the contract address for Roll Token 2 https://roll.calderaexplorer.xyz/token/0x9ba02D2A38c8aCA7E0779DF2F3808848b20aFe61/token-transfers
+        address: "0x9ba02D2A38c8aCA7E0779DF2F3808848b20aFe61",
+      },
+      assets: new Map([["erc20", { file: "./abis/erc20.abi.json" }]]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Call,
+            handler: "handleTransaction",
+            filter: {
+              /**
+               * The function can either be the function fragment or signature
+               * function: '0x095ea7b3'
+               * function: '0x7ff36ab500000000000000000000000000000000000000000000000000000000'
+               */
+              function: "approve(address spender, uint256 rawAmount)",
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleLog",
+            filter: {
+              /**
+               * Follows standard log filters https://docs.ethers.io/v5/concepts/events/
+               * address: "0x60781C2586D68229fde47564546784ab3fACA982"
+               */
+              topics: [
+                "Transfer(address indexed from, address indexed to, uint256 amount)",
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
