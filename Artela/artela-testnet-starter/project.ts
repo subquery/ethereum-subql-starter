6853e10695a0c65b0d37d5e141b9e1572fafd404
James Bayly <46693720+jamesbayly@users.noreply.github.com>
2023-12-07 11:28:42 +0800

    Update Manifest Files (#59)
    * Remove dictionaries
    
    * Note rate limit controls

--- Artela/artela-testnet-starter/project.ts
+++ Artela/artela-testnet-starter/project.ts
@@ -31,10 +31,12 @@ const project: EthereumProject = {
      */
     chainId: "11820",
     /**
-     * This endpoint should be a public non-pruned archive node
+     * These endpoint(s) should be public non-pruned archive node
      * We recommend providing more than one endpoint for improved reliability, performance, and uptime
      * Public nodes may be rate limited, which can affect indexing speed
      * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
     endpoint: [
       "https://testnet-rpc1.artela.network",
--- Meter/meter-starter/project.ts
+++ Meter/meter-starter/project.ts
@@ -35,11 +35,10 @@ const project: EthereumProject = {
      * We recommend providing more than one endpoint for improved reliability, performance, and uptime
      * Public nodes may be rate limited, which can affect indexing speed
      * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
     endpoint: ["https://rpc-meter.jellypool.xyz/"],
-
-    // Recommended to provide the HTTP endpoint of a full chain dictionary to speed up processing
-    // dictionary: "https://gx.api.subquery.network/sq/subquery/meter-dictionary"
   },
   dataSources: [
     {
