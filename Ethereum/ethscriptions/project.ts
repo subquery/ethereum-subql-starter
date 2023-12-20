599f430acdd27696a4a42cfe15a7df47b5544f94
godlin <bogodline@gmail.com>
2023-12-20 20:25:48 +0100

    Ethscriptions

---
+++ Ethereum/ethscriptions/project.ts
@@ -0,0 +1,62 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "ethereum-subql-starter",
+  description:
+    "This project can be use as a starting point for developing your new Ethereum SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the EVM Chain ID, for Ethereum this is 1
+     * https://chainlist.org/chain/1
+     */
+    chainId: "1",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: ["https://rpc.ankr.com/eth"],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 18130011,
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Call,
+            handler: "handleTransaction",
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
---
+++ Ethereum/ethscriptions/src/mappings/mappingHandlers.ts
@@ -0,0 +1,62 @@
+// import { TypeormDatabase } from "@subsquid/typeorm-store";
+// import { Inscription } from "./model";
+// import { processor } from "./processor";
+import assert from "assert";
+import { Inscription } from "../types";
+import { isValidDataUri, hexToUTF8 } from "./utils";
+import { EthereumTransaction } from "@subql/types-ethereum";
+
+export async function handleTransaction(
+  tx: EthereumTransaction
+): Promise<void> {
+  logger.warn(tx.hash);
+  const knownInscriptionsArray: Inscription[] | undefined =
+    await Inscription.getByIsEsip6(false);
+  assert(knownInscriptionsArray);
+  const knownInscriptions: Map<string, Inscription> = new Map(
+    knownInscriptionsArray.map((c) => [c.data, c])
+  );
+  const newUniqueInscriptions: Map<string, Inscription> = new Map();
+  const inscriptions: Inscription[] = [];
+  if (tx.to) {
+    //is unique or has rule=esip6
+
+    const decodedData = hexToUTF8(tx.input);
+    //console.log(decodedData);
+    if (isValidDataUri(decodedData)) {
+      if (decodedData.includes("rule=esip6")) {
+        inscriptions.push(
+          Inscription.create({
+            id: tx.hash,
+            data: decodedData,
+            block: tx.blockNumber,
+            creator: tx.from,
+            isEsip6: true,
+          })
+        );
+      } else if (
+        !knownInscriptions.has(decodedData) &&
+        !newUniqueInscriptions.has(decodedData)
+      ) {
+        //console.log("new");
+        newUniqueInscriptions.set(
+          decodedData,
+          Inscription.create({
+            id: tx.hash,
+            data: decodedData,
+            block: tx.blockNumber,
+            creator: tx.from,
+            isEsip6: false,
+          })
+        );
+      }
+      //checkuniquesness
+    }
+  }
+  inscriptions.map(async (inscription) => await inscription.save());
+  if (newUniqueInscriptions.size > 0) {
+    newUniqueInscriptions.forEach(async (value, key) => {
+      await value.save();
+    });
+  }
+}
