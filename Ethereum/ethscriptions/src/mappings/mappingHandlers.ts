7fec7c2c9b89ca7e27455001f99eaeb11c08c31e
godlin <bogodline@gmail.com>
2024-01-04 22:47:38 +0100

    Ethscriptions Example Added

---
+++ Ethereum/ethscriptions/src/mappings/mappingHandlers.ts
@@ -0,0 +1,24 @@
+import { Inscription } from "../types";
+import { isValidDataUri, hexToUTF8 } from "./utils";
+import { EthereumTransaction } from "@subql/types-ethereum";
+
+export async function handleTransaction(
+  tx: EthereumTransaction
+): Promise<void> {
+  if (tx.to) {
+    if (!tx.input) {
+      return;
+    }
+    let inscription: Inscription;
+    const decodedData = hexToUTF8(tx.input);
+    if (isValidDataUri(decodedData)) {
+      inscription = Inscription.create({
+        id: tx.hash,
+        data: decodedData,
+        block: tx.blockNumber,
+        creator: tx.from,
+      });
+      inscription.save();
+    }
+  }
+}
