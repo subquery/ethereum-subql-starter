facd42193155ac71015eeefd3725ded520a1b8bc
James Bayly <bayly.james@gmail.com>
2024-01-12 09:42:09 +0800

    Add x1 testnet (#61)

---
+++ X1/x1-testnet-starter/project.ts
@@ -0,0 +1,93 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "x1-testnet-subql-starter",
+  description:
+    "This project can be use as a starting point for developing your new X1 Testnet SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the EVM Chain ID, for X1 Testnet this is 195
+     * https://chainlist.org/chain/195
+     */
+    chainId: "195",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: ["https://testrpc.x1.tech"],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 5462000,
+      // Usually you would set this to the block that the contract was deployed on https://www.oklink.com/x1-test/address/0xbec7859bc3d0603bec454f7194173e36bf2aa5c8
+      // However on this RPC, they prune all logs older than the most recent 10000 blocks
+      // When running, either use an archival node that has no pruning, or update this value to be within the last 10000 blocks
+      options: {
+        // Must be a key of assets
+        abi: "erc20",
+        // this is the contract address for wrapped ether https://www.oklink.com/x1-test/token/0xbec7859bc3d0603bec454f7194173e36bf2aa5c8
+        address: "0xbec7859bc3d0603bec454f7194173e36bf2aa5c8",
+      },
+      assets: new Map([["erc20", { file: "./abis/erc20.abi.json" }]]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Call,
+            handler: "handleTransaction",
+            filter: {
+              /**
+               * The function can either be the function fragment or signature
+               * function: '0x095ea7b3'
+               * function: '0x7ff36ab500000000000000000000000000000000000000000000000000000000'
+               */
+              function: "approve(address spender, uint256 rawAmount)",
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleLog",
+            filter: {
+              /**
+               * Follows standard log filters https://docs.ethers.io/v5/concepts/events/
+               * address: "0x60781C2586D68229fde47564546784ab3fACA982"
+               */
+              topics: [
+                "Transfer(address indexed from, address indexed to, uint256 amount)",
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
