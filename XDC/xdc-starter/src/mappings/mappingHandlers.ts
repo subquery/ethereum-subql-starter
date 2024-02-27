1b7af7ee8a57f03617ace81d72dffafafcbace37
godlin <bogodline@gmail.com>
2024-02-27 14:17:33 +0100

    XDC featured

---
+++ XDC/xdc-starter/src/mappings/mappingHandlers.ts
@@ -0,0 +1,37 @@
+import { Approval, Transfer } from "../types";
+import {
+  ApproveTransaction,
+  TransferLog,
+} from "../types/abi-interfaces/Erc20Abi";
+import assert from "assert";
+
+export async function handleLog(log: TransferLog): Promise<void> {
+  logger.info(`New transfer transaction log at block ${log.blockNumber}`);
+  assert(log.args, "No log.args");
+
+  const transaction = Transfer.create({
+    id: log.transactionHash,
+    blockHeight: BigInt(log.blockNumber),
+    to: log.args.to,
+    from: log.args.from,
+    value: log.args.value.toBigInt(),
+    contractAddress: log.address,
+  });
+
+  await transaction.save();
+}
+
+export async function handleTransaction(tx: ApproveTransaction): Promise<void> {
+  logger.info(`New Approval transaction at block ${tx.blockNumber}`);
+  assert(tx.args, "No tx.args");
+
+  const approval = Approval.create({
+    id: tx.hash,
+    owner: tx.from,
+    spender: await tx.args[0],
+    value: BigInt(await tx.args[1].toString()),
+    contractAddress: tx.to,
+  });
+
+  await approval.save();
+}
