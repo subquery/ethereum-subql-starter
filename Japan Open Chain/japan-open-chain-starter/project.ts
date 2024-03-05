dc79a653900fe3e7228a9652dec446b276a1dcc9
James Bayly <james@bayly.xyz>
2024-03-05 09:48:36 +0800

    Add Japan Open Chain

---
+++ Japan Open Chain/japan-open-chain-starter/project.ts
@@ -0,0 +1,93 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "japan-open-chain-starter",
+  description:
+    "This project can be use as a starting point for developing your new Japan Open Chain SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the EVM Chain ID, for Japan Open Chain this is 81
+     * https://chainlist.org/chain/81
+     */
+    chainId: "81",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: [
+      "https://rpc-1.japanopenchain.org:8545",
+      "https://rpc-2.japanopenchain.org:8545",
+    ],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      // Block height at which the smart contract was deployed
+      startBlock: 5278626,
+
+      options: {
+        // Must be a key of assets
+        abi: "erc20",
+        // # this is the contract address for wrapped ether https://mainnet.japanopenchain.org/address/0x090653A88144038a713262f5eE8661b0C25831F4
+        address: "0x090653A88144038a713262f5eE8661b0C25831F4",
+      },
+      assets: new Map([["erc20", { file: "./abis/erc20.abi.json" }]]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Call,
+            handler: "handleTransaction",
+            filter: {
+              /**
+               * The function can either be the function fragment or signature
+               * function: '0x095ea7b3'
+               * function: '0x7ff36ab500000000000000000000000000000000000000000000000000000000'
+               */
+              function: "approve(address spender, uint256 rawAmount)",
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleLog",
+            filter: {
+              /**
+               * Follows standard log filters https://docs.ethers.io/v5/concepts/events/
+               * address: "0x60781C2586D68229fde47564546784ab3fACA982"
+               */
+              topics: ["Transfer(address,address,uint256)"],
+            },
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
