6c99a367b31e836a00e0356520fe2afb523c475b
James Bayly <james@bayly.xyz>
2024-03-05 13:11:23 +0800

    Add Astar ZKEVM Mainnet

--- Astar/astar-zkevm-starter/project.ts
+++ Astar/astar-zkevm-starter/project.ts
@@ -8,9 +8,9 @@ import {
 const project: EthereumProject = {
   specVersion: "1.0.0",
   version: "0.0.1",
-  name: "astar-zkevm-testnet-starter",
+  name: "astar-zkevm-starter",
   description:
-    "This project can be use as a starting point for developing your new Astar zkEVM Testnet SubQuery project",
+    "This project can be use as a starting point for developing your new Astar zkEVM SubQuery project",
   runner: {
     node: {
       name: "@subql/node-ethereum",
@@ -26,10 +26,10 @@ const project: EthereumProject = {
   },
   network: {
     /**
-     * chainId is the EVM Chain ID, for Astar zkEVM zKatana testnet this is 1261120
-     * https://chainlist.org/chain/1261120
+     * chainId is the EVM Chain ID, for Astar zkEVM this is 3776
+     * https://chainlist.org/chain/3776
      */
-    chainId: "1261120",
+    chainId: "3776",
     /**
      * These endpoint(s) should be public non-pruned archive node
      * We recommend providing more than one endpoint for improved reliability, performance, and uptime
@@ -38,18 +38,18 @@ const project: EthereumProject = {
      * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
      * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
-    endpoint: ["https://rpc.zkatana.gelato.digital"],
+    endpoint: ["https://rpc.startale.com/astar-zkevm"],
     // Recommended to provide the HTTP endpoint of a full chain dictionary to speed up processing
   },
   dataSources: [
     {
       kind: EthereumDatasourceKind.Runtime,
-      startBlock: 53, // This is the block that the contract was deployed on https://zkatana.blockscout.com/tx/0x625fd9f365a1601486c4176bc34cf0fdf04bf06b2393fd5dd43e8dd7a62d9ec5
+      startBlock: 101596, // This is the block that the contract was deployed on https://astar-zkevm.explorer.startale.com/tx/0x7035568194509ffe6e7d4707ce34bf7523a98a7b7f8a580ea7321d9849f995c9
       options: {
         // Must be a key of assets
         abi: "erc20",
-        // This is the contract address for GACHA Token https://zkatana.blockscout.com/token/0x28687c2A4638149745A0999D523f813f63b4786F
-        address: "0x28687c2A4638149745A0999D523f813f63b4786F",
+        // This is the contract address for USDC Token https://astar-zkevm.explorer.startale.com/address/0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035
+        address: "0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035",
       },
       assets: new Map([["erc20", { file: "./abis/erc20.abi.json" }]]),
       mapping: {
