5e644e82490060ad46f07db61f5703ee84b9ad21
Scott Twiname <skott.twiname@gmail.com>
2024-03-07 13:54:49 +1300

    Add base sepolia starter (#82)
    * Add base sepolia starter
    
    * Update Base/base-sepolia-starter/README.md
    
    * Update Base/base-sepolia-starter/package.json
    
    * Update Base/base-sepolia-starter/README.md
    
    ---------
    
    Co-authored-by: James Bayly <bayly.james@gmail.com>

---
+++ Base/base-sepolia-starter/project.ts
@@ -0,0 +1,91 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "base-sepolia-starter",
+  description:
+    "This project can be use as a starting point for developing your new Base Sepolia SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     *  chainId is the EVM Chain ID, for Base Sepolia this is 84531
+     *  https://chainlist.org/chain/84532
+     */
+    chainId: "84532",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: ["https://sepolia.base.org"],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 6958775,
+
+      options: {
+        // Must be a key of assets
+        abi: "erc20",
+        // This is the block that the contract was deployed on https://sepolia.basescan.org/token/0xe377517b4d95555052346c789e5b2b1cf8e4189e
+        address: "0xE377517b4D95555052346C789E5b2b1CF8E4189e",
+      },
+      assets: new Map([["erc20", { file: "./abis/erc20.abi.json" }]]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Call,
+            handler: "handleTransaction",
+            filter: {
+              /**
+               * The function can either be the function fragment or signature
+               * function: '0x095ea7b3'
+               * function: '0x7ff36ab500000000000000000000000000000000000000000000000000000000'
+               */
+              function: "approve(address spender, uint256 rawAmount)",
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleLog",
+            filter: {
+              /**
+               * Follows standard log filters https://docs.ethers.io/v5/concepts/events/
+               * address: "0x60781C2586D68229fde47564546784ab3fACA982"
+               */
+              topics: [
+                "Transfer(address indexed from, address indexed to, uint256 amount)",
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
