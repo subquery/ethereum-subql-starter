7d15ed0f54f0340e09cbc06f59347cac553873b7
James Bayly <james@bayly.xyz>
2024-03-28 14:00:30 +1300

    Poap ethereum test

---
+++ Ethereum/ethereum-poap/project.ts
@@ -0,0 +1,90 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "gnosis-poap",
+  description:
+    "This project is a SubQuery Version of the Ethereum Poap Subgraph https://github.com/poap-xyz/poap-subgraph",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the EVM Chain ID, for Ethereum this is 1
+     * https://chainlist.org/chain/1
+     */
+    chainId: "1",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: ["https://ethereum.rpc.subquery.network/public"],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      // When the POAP contract was deployed
+      startBlock: 7844214,
+      options: {
+        // Must be a key of assets
+        abi: "poap",
+        // this is the contract address for POAPs on Ethereum https://etherscan.io/address/0x22C1f6050E56d2876009903609a2cC3fEf83B415
+        address: "0x22C1f6050E56d2876009903609a2cC3fEf83B415",
+      },
+      assets: new Map([["poap", { file: "./abis/poap.abi.json" }]]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleEventToken",
+            filter: {
+              /**
+               * Follows standard log filters https://docs.ethers.io/v5/concepts/events/
+               * address: "0x60781C2586D68229fde47564546784ab3fACA982"
+               */
+              topics: ["EventToken(uint256 eventId, uint256 tokenId)"],
+            },
+          },
+          {
+            kind: EthereumHandlerKind.Event,
+            handler: "handleTransfer",
+            filter: {
+              /**
+               * Follows standard log filters https://docs.ethers.io/v5/concepts/events/
+               * address: "0x60781C2586D68229fde47564546784ab3fACA982"
+               */
+              topics: [
+                "Transfer(address indexed from, address indexed to, uint256 amount)",
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
---
+++ Ethereum/ethereum-poap/src/mappings/mappingHandlers.ts
@@ -0,0 +1,107 @@
+import assert from "assert";
+import { Token, Account, Event, Transfer } from "../types";
+import { EventTokenEvent, TransferEvent } from "../types/contracts/PoapAbi";
+import { EthereumLog } from "@subql/types-ethereum";
+
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+function createEventID(event: TransferEvent): string {
+  return event.blockNumber
+    .toString()
+    .concat("-")
+    .concat(event.logIndex.toString());
+}
+
+export async function handleEventToken(ev: EventTokenEvent): Promise<void> {
+  // This handler always run after the transfer handler
+  logger.info(`New event token at block ${ev.blockNumber}`);
+  let event = await Event.get(ev.args.eventId.toString().toLowerCase());
+  if (event == null) {
+    event = Event.create({
+      id: ev.args.eventId.toString().toLowerCase(),
+      tokenCount: BigInt(1),
+      tokenMints: BigInt(1),
+      transferCount: BigInt(1),
+      created: BigInt((await ev.getBlock()).timestamp),
+    });
+  }
+
+  let token = await Token.get(ev.args.tokenId.toString());
+  if (token) {
+    token.eventId = event.id;
+    token.mintOrder = event.tokenMints;
+    await Promise.all([event.save(), token.save()]);
+  } else {
+    await event.save();
+  }
+}
+
+export async function handleTransfer(ev: TransferEvent): Promise<void> {
+  logger.info(`New transfer at block ${ev.blockNumber}`);
+  let token = await Token.get(ev.args.tokenId.toString());
+  let from = await Account.get(ev.args.from);
+  let to = await Account.get(ev.args.to);
+
+  if (from == null) {
+    from = Account.create({
+      id: ev.args.from,
+      tokensOwned: BigInt(1), // The from account at least has to own one token
+    });
+  }
+
+  // Don't subtracts from the ZERO_ADDRESS (it's the one that mint the token)
+  // Avoid negative values
+  if (from.id != ZERO_ADDRESS) {
+    from.tokensOwned -= BigInt(0);
+  }
+
+  if (to == null) {
+    to = Account.create({
+      id: ev.args.to,
+      tokensOwned: BigInt(0), // The from account at least has to own one token
+    });
+  }
+  to.tokensOwned += BigInt(1);
+
+  if (token == null) {
+    token = Token.create({
+      id: ev.args.tokenId.toString(),
+      transferCount: BigInt(0),
+      created: BigInt((await ev.getBlock()).timestamp),
+      ownerId: to.id,
+    });
+  }
+  token.ownerId = to.id;
+  token.transferCount += BigInt(1);
+
+  let transfer = Transfer.create({
+    id: createEventID(ev),
+    tokenId: token.id,
+    fromId: from.id,
+    toId: to.id,
+    transaction: ev.transactionHash,
+    timestamp: BigInt((await ev.getBlock()).timestamp),
+    blockheight: BigInt(ev.blockNumber),
+  });
+
+  // Save from and to
+  await Promise.all([token.save(), from.save(), to.save(), transfer.save()]);
+
+  let event: Event | undefined = undefined;
+  if (token.eventId) {
+    event = await Event.get(token.eventId);
+  }
+
+  if (event != undefined) {
+    // Add one transfer
+    event.transferCount += BigInt(1);
+
+    // Burning the token
+    if (to.id == ZERO_ADDRESS) {
+      event.tokenCount -= BigInt(1);
+      // Subtract all the transfers from the burned token
+      event.transferCount -= token.transferCount;
+    }
+    await event.save();
+  }
+}
