aaf202a0cffff16e383d23738719f17758436904
godlin <bogodline@gmail.com>
2024-04-11 16:54:45 +0200

    zKyoto Featured

---
+++ Astar/astar-zkyoto-testnet-starter/project.ts
@@ -0,0 +1,63 @@
+import {
+  EthereumProject,
+  EthereumDatasourceKind,
+  EthereumHandlerKind,
+} from "@subql/types-ethereum";
+
+// Can expand the Datasource processor types via the generic param
+const project: EthereumProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "zkyoto-testnet-starter",
+  description:
+    "This project can be use as a starting point for developing your new zKyoto Testnet SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-ethereum",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /**
+     * chainId is the EVM Chain ID, for zKyoto Testnet this is 6038361
+     * https://chainlist.org/chain/6038361
+     */
+    chainId: "6038361",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
+     * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
+     */
+    endpoint: ["https://rpc.startale.com/zkyoto"],
+  },
+  dataSources: [
+    {
+      kind: EthereumDatasourceKind.Runtime,
+      startBlock: 1,
+      assets: new Map([["erc20", { file: "./abis/erc20.abi.json" }]]),
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            kind: EthereumHandlerKind.Call,
+            handler: "handleTransaction",
+          },
+        ],
+      },
+    },
+  ],
+  repository: "https://github.com/subquery/ethereum-subql-starter",
+};
+
+// Must set default to the project instance
+export default project;
---
+++ Astar/astar-zkyoto-testnet-starter/src/mappings/mappingHandlers.ts
@@ -0,0 +1,16 @@
+import { Transaction } from "../types";
+import assert from "assert";
+import { EthereumTransaction } from "@subql/types-ethereum";
+
+export async function handleTransaction(
+  tx: EthereumTransaction
+): Promise<void> {
+  const approval = Transaction.create({
+    id: tx.hash,
+    to: tx.to,
+    from: tx.from,
+    blockHeight: BigInt(tx.blockNumber),
+  });
+
+  await approval.save();
+}
