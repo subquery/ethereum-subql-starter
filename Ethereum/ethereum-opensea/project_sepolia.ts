f7e9a45f33a96424bfc5abea1573d1e49256936d
Scott Twiname <skott.twiname@gmail.com>
2024-05-20 22:19:59 +1200

    Use subquery network endpoints for supported networks

--- Ethereum/ethereum-opensea/project_sepolia.ts
+++ Ethereum/ethereum-opensea/project_sepolia.ts
@@ -38,7 +38,7 @@ const project: EthereumProject = {
      * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
      * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
-    endpoint: ["https://eth-sepolia.public.blastapi.io"],
+    endpoint: ["https://ethereum-sepolia.rpc.subquery.network/public"],
   },
   dataSources: [
     {
