732c512a6fcc2effb1e477b183bf01a7a7dab9ec
Scott Twiname <skott.twiname@gmail.com>
2024-05-20 23:18:50 +1200

    Use subquery network endpoints for supported networks (#120)

--- Ethereum/ethereum-sepolia-starter/project.ts
+++ Ethereum/ethereum-sepolia-starter/project.ts
@@ -38,7 +38,7 @@ const project: EthereumProject = {
      * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
      * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
-    endpoint: ["wss://sepolia.gateway.tenderly.co"],
+    endpoint: ["https://ethereum-sepolia.rpc.subquery.network/public"],
   },
   dataSources: [
     {
--- Polygon/polygon-plasma-bridge/project.ts
+++ Polygon/polygon-plasma-bridge/project.ts
@@ -38,7 +38,7 @@ const project: EthereumProject = {
      * If you use a rate limited endpoint, adjust the --batch-size and --workers parameters
      * These settings can be found in your docker-compose.yaml, they will slow indexing but prevent your project being rate limited
      */
-    endpoint: "https://polygon.api.onfinality.io/public",
+    endpoint: "https://polygon.rpc.subquery.network/public",
   },
   dataSources: [
     {
