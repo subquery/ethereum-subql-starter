957e006a81958096082a67788aa0b3eb4f61ceaf
James Bayly <bayly.james@gmail.com>
2024-05-22 08:43:36 +0800

    Update ONIGIRI/onigiri-starter/project.ts

--- ONIGIRI/onigiri-starter/project.ts
+++ ONIGIRI/onigiri-starter/project.ts
@@ -8,7 +8,7 @@ import {
 const project: EthereumProject = {
   specVersion: "1.0.0",
   version: "0.0.1",
-  name: "onigiri-testnet-starter",
+  name: "onigiri-starter",
   description:
     "This project can be use as a starting point for developing your new ONIGIRI SubQuery project",
   runner: {
